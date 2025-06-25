afb0cc1d4d8526dd09b1926be0be51e527b26426
Nicolas Lyot <nicolas.lyot@hygeos.com>
2025-06-25 13:39:04 +0200

    Feat: Dev on modis reader
    premier jet modis

--- eoread/modis.py
+++ eoread/modis.py
@@ -16,46 +16,46 @@ import dask.array as da
 # bands_vis = [410,440,485,530,550,668,670,680,685,750,870,900,935,940,1375]
 # bands_tir = [3750,3960,4050,4460,4510,1375,6710,7230,8550,9730,11000,12000,13230,13630,13930,14230]
 
-# band_index = { # Bands      - wavelength (um)   - resolution (m)    - group
-#     650:  1,   # Band 1        0.62 - 0.67	         250              250m
-#     860:  2,   # Band 2        0.84 - 0.87	         250              250m
-#     470:  3,   # Band 3        0.46 - 0.48	         500              500m
-#     555:  4,   # Band 4        0.54 - 0.56           500              500m
-#     1240: 5,   # Band 5   	   1.23 - 1.25	         500              500m
-#     1640: 6,   # Band 6   	   1.63 - 1.65	         500              500m
-#     2130: 7,   # Band 7   	   2.11 - 2.16	         500              500m
-#     410:  8,   # Band 8   	   0.40 - 0.42	         1000            Ref_1km
-#     440:  9,   # Band 9   	   0.44 - 0.45	         1000            Ref_1km
-#     485:  10,  # Band 10  	   0.48 - 0.49	         1000            Ref_1km
-#     530:  11,  # Band 11  	   0.52 - 0.53 	         1000            Ref_1km
-#     550:  12,  # Band 12  	   0.54 - 0.56	         1000            Ref_1km
-#     670:  13,  # Band 13  	   0.66 - 0.67	         1000            Ref_1km
-#     672:  13.5,# Band 13  	   0.66 - 0.67	         1000            Ref_1km
-#     680:  14,  # Band 14  	   0.67 - 0.68	         1000            Ref_1km
-#     682:  14.5,# Band 14  	   0.67 - 0.68	         1000            Ref_1km
-#     750:  15,  # Band 15  	   0.74 - 0.75 	         1000            Ref_1km
-#     870:  16,  # Band 16  	   0.86 - 0.88 	         1000            Ref_1km
-#     900:  17,  # Band 17  	   0.89 - 0.92	         1000            Ref_1km
-#     935:  18,  # Band 18  	   0.93 - 0.94	         1000            Ref_1km
-#     940:  19,  # Band 19  	   0.91 - 0.96	         1000            Ref_1km
-#     3750: 20,  # Band 20  	   3.66 - 3.84	         1000            Emi_1km
-#     3960: 21,  # Band 21  	   3.93 - 3.99	         1000            Emi_1km
-#     3962: 22,  # Band 22  	   3.93 - 3.99	         1000            Emi_1km
-#     4050: 23,  # Band 23  	   4.02 - 4.08	         1000            Emi_1km
-#     4460: 24,  # Band 24  	   4.43 - 4.50	         1000            Emi_1km
-#     4510: 25,  # Band 25       4.48 - 4.55	         1000            Emi_1km
-#     1375: 26,  # Band 26 	   1.36 - 1.39	         1000            Ref_1km
-#     6710: 27,  # Band 27 	   6.53 - 6.89	         1000            Emi_1km
-#     7230: 28,  # Band 28  	   7.17 - 7.47	         1000            Emi_1km
-#     8550: 29,  # Band 29  	   8.40 - 8.70	         1000            Emi_1km
-#     9730: 30,  # Band 30  	   9.58 - 9.88	         1000            Emi_1km
-#     11000:31,  # Band 31  	   10.78 - 10.28         1000            Emi_1km
-#     12000:32,  # Band 32  	   11.77 - 12.27         1000            Emi_1km
-#     13230:33,  # Band 33       13.18 - 13.48         1000            Emi_1km
-#     13630:34,  # Band 34       13.48 - 13.78         1000            Emi_1km
-#     13930:35,  # Band 35       13.78 - 14.08         1000            Emi_1km
-#     14230:36,  # Band 36       14.08 - 14.38         1000            Emi_1km
-#     }
+band_index = { # Bands      - wavelength (um)   - resolution (m)    - group
+    650:  1,   # Band 1        0.62 - 0.67	         250              250m
+    860:  2,   # Band 2        0.84 - 0.87	         250              250m
+    470:  3,   # Band 3        0.46 - 0.48	         500              500m
+    555:  4,   # Band 4        0.54 - 0.56           500              500m
+    1240: 5,   # Band 5   	   1.23 - 1.25	         500              500m
+    1640: 6,   # Band 6   	   1.63 - 1.65	         500              500m
+    2130: 7,   # Band 7   	   2.11 - 2.16	         500              500m
+    410:  8,   # Band 8   	   0.40 - 0.42	         1000            Ref_1km
+    440:  9,   # Band 9   	   0.44 - 0.45	         1000            Ref_1km
+    485:  10,  # Band 10  	   0.48 - 0.49	         1000            Ref_1km
+    530:  11,  # Band 11  	   0.52 - 0.53 	         1000            Ref_1km
+    550:  12,  # Band 12  	   0.54 - 0.56	         1000            Ref_1km
+    670:  13,  # Band 13  	   0.66 - 0.67	         1000            Ref_1km
+    672:  13.5,# Band 13  	   0.66 - 0.67	         1000            Ref_1km
+    680:  14,  # Band 14  	   0.67 - 0.68	         1000            Ref_1km
+    682:  14.5,# Band 14  	   0.67 - 0.68	         1000            Ref_1km
+    750:  15,  # Band 15  	   0.74 - 0.75 	         1000            Ref_1km
+    870:  16,  # Band 16  	   0.86 - 0.88 	         1000            Ref_1km
+    900:  17,  # Band 17  	   0.89 - 0.92	         1000            Ref_1km
+    935:  18,  # Band 18  	   0.93 - 0.94	         1000            Ref_1km
+    940:  19,  # Band 19  	   0.91 - 0.96	         1000            Ref_1km
+    3750: 20,  # Band 20  	   3.66 - 3.84	         1000            Emi_1km
+    3960: 21,  # Band 21  	   3.93 - 3.99	         1000            Emi_1km
+    3962: 22,  # Band 22  	   3.93 - 3.99	         1000            Emi_1km
+    4050: 23,  # Band 23  	   4.02 - 4.08	         1000            Emi_1km
+    4460: 24,  # Band 24  	   4.43 - 4.50	         1000            Emi_1km
+    4510: 25,  # Band 25       4.48 - 4.55	         1000            Emi_1km
+    1375: 26,  # Band 26 	   1.36 - 1.39	         1000            Ref_1km
+    6710: 27,  # Band 27 	   6.53 - 6.89	         1000            Emi_1km
+    7230: 28,  # Band 28  	   7.17 - 7.47	         1000            Emi_1km
+    8550: 29,  # Band 29  	   8.40 - 8.70	         1000            Emi_1km
+    9730: 30,  # Band 30  	   9.58 - 9.88	         1000            Emi_1km
+    11000:31,  # Band 31  	   10.78 - 10.28         1000            Emi_1km
+    12000:32,  # Band 32  	   11.77 - 12.27         1000            Emi_1km
+    13230:33,  # Band 33       13.18 - 13.48         1000            Emi_1km
+    13630:34,  # Band 34       13.48 - 13.78         1000            Emi_1km
+    13930:35,  # Band 35       13.78 - 14.08         1000            Emi_1km
+    14230:36,  # Band 36       14.08 - 14.38         1000            Emi_1km
+    }
 
 bands = [650, 860, 470, 555, 1240, 1640, 2130, 410, 440, 485, 530, 550, 670, 672, 
          680, 682, 750, 870, 900, 935, 940, 3750, 3960, 3962, 4050, 4460, 4510, 
@@ -103,32 +103,31 @@ def Level1_MODIS(filepath: Path | str, chunks: int = 500):
     new_coords = {}
     
     coords = l1.coords
-    new_dims = new_dims + [n.bands,n.bands_tir]
-    old_dims = old_dims + ['bands_vis','bands_bt']
-    new_coords[n.bands_tir] = [reverse_band_index[i] for i in coords['Band_1KM_Emissive'].values]
-    new_coords[n.bands] = [reverse_band_index[i] for i in coords['Band_250M'].values] + \
-                            [reverse_band_index[i] for i in coords['Band_500M'].values] + \
-                            [reverse_band_index[i] for i in coords['Band_1KM_RefSB'].values]
+    new_dims = new_dims + [n.bands.name,n.bands_ir.name]
+    old_dims = old_dims + ['bands_toa','bands_bt']
+    new_coords[n.bands_ir.name] = [reverse_band_index[i] for i in coords['bands_1KM_Emissive'].values]
+    new_coords[n.bands.name] = [reverse_band_index[i] for i in coords['bands_250M'].values] + \
+                            [reverse_band_index[i] for i in coords['bands_500M'].values] + \
+                            [reverse_band_index[i] for i in coords['bands_1KM_RefSB'].values]
     
     revize_dims = dict(zip(old_dims, new_dims))
     l1 = l1.rename_dims(revize_dims)
     l1 = l1.assign_coords(new_coords)
 
     # Summarize Attributes
-    list_attr = [attr.split("=") for attr in l1.attrs['CoreMetadata.0'].split('\n') if len(attr) != 0]
-    attributes = {}
+    attributes = l1.attrs['CoreMetadata.0']
     l1.attrs = {}
-    parse_attrs(list_attr, attributes)
+    #parse_attrs(list_attr, attributes)
     l1.attrs[n.input_directory] = str(filepath.parent)
     l1.attrs[n.resolution]   = 1000
     l1.attrs[n.datetime]     = attributes['INVENTORYMETADATA']['ECSDATAGRANULE']['PRODUCTIONDATETIME'][1:-1]
     l1.attrs['night']        = str(attributes['INVENTORYMETADATA']['ECSDATAGRANULE']['DAYNIGHTFLAG'] != '"Day"')
     l1.attrs[n.product_name] = attributes['INVENTORYMETADATA']['ECSDATAGRANULE']['LOCALGRANULEID'][1:-1]
-    l1.attrs[n.platform]     = attributes['ASSOCIATEDPLATFORMINSTRUMENTSENSOR']['ASSOCIATEDPLATFORMSHORTNAME'][1:-1]
-    l1.attrs[n.sensor]       = attributes['ASSOCIATEDPLATFORMINSTRUMENTSENSOR']['ASSOCIATEDPLATFORMINSTRUMENTSENSORCONTAINER'][1:-1]
-    l1.attrs[n.shortname]    = attributes['INVENTORYMETADATA']['COLLECTIONDESCRIPTIONCLASS']['SHORTNAME'][1:-1]
-    l1.attrs['version']      = int(attributes['INVENTORYMETADATA']['COLLECTIONDESCRIPTIONCLASS']['VERSIONID'])
-
+    #l1.attrs[n.platform]     = attributes['ASSOCIATEDPLATFORMINSTRUMENTSENSOR']['ASSOCIATEDPLATFORMSHORTNAME'][1:-1]
+    #l1.attrs[n.sensor]       = attributes['ASSOCIATEDPLATFORMINSTRUMENTSENSOR']['ASSOCIATEDPLATFORMINSTRUMENTSENSORCONTAINER'][1:-1]
+    #l1.attrs[n.shortname]    = attributes['INVENTORYMETADATA']['COLLECTIONDESCRIPTIONCLASS']['SHORTNAME'][1:-1]
+    #l1.attrs['version']      = int(attributes['INVENTORYMETADATA']['COLLECTIONDESCRIPTIONCLASS']['VERSIONID'])
+    print()
     return l1
 
 def transform_radiometry(level1):
@@ -139,57 +138,59 @@ def transform_radiometry(level1):
     # tag_offset = f'{radiometry}_offsets'
     # tag_unit   = f'{radiometry}_units'
     # rad_varnames = ['EV_250_Aggr1km_RefSB','EV_500_Aggr1km_RefSB','EV_1KM_RefSB','EV_1KM_Emissive']
+    # rad_bandnames = ["new_bands_250M:MODIS_SWATH_Type_L1B", "new_bands_500M:MODIS_SWATH_Type_L1B", "new_bands_1KM_RefSB:MODIS_SWATH_Type_L1B", "new_bands_1KM_Emissive:MODIS_SWATH_Type_L1B"]
+    rad_varnames = ['EV_250_Aggr1km_RefSB','EV_500_Aggr1km_RefSB','EV_1KM_RefSB'] #TOA
+    rad_bandnames = ["new_bands_250M:MODIS_SWATH_Type_L1B", "new_bands_500M:MODIS_SWATH_Type_L1B", "new_bands_1KM_RefSB:MODIS_SWATH_Type_L1B"]
     
+    #bt is EV_1KM_Emissive
     # Compute radiance
+    """
     rad = level1['EV_1KM_Emissive']
     band_dim = 'new_Band_1KM_Emissive:MODIS_SWATH_Type_L1B'
     scales = xr.DataArray(rad.radiance_scales, dims=band_dim)
     offset = xr.DataArray(rad.radiance_offsets, dims=band_dim)
     level1[n.ltoa.name] = scales * rad + offset
+    """
+    data_arrays = []
+
+    for rad_varname, rad_bandname in zip(rad_varnames, rad_bandnames):
+        rad = level1[rad_varname]
+
+        # Broadcast scales and offsets with appropriate dimensions
+        scale = xr.DataArray(rad.radiance_scales, dims=rad_bandname)
+        offset = xr.DataArray(rad.radiance_offsets, dims=rad_bandname)
+
+        # Expand scale/offset to match rad's dims for broadcasting
+        scale = scale.broadcast_like(rad)
+        offset = offset.broadcast_like(rad)
+
+        nd = scale * rad + offset
+        nd.attrs['desc'] = n.ltoa.desc
+        nd.attrs['unit'] = n.ltoa.unit
+        nd = nd.rename({rad_bandname : "bands_toa"})
+        data_arrays.append(nd)
+
+    # Combine into one dimension
+    level1[n.ltoa.name] = xr.concat(data_arrays, dim="bands_toa")
     
     # Compute Reflectance
-    # Compute Brightness temperature 
+    emi_b = level1['EV_1KM_Emissive']
+    band_dim = 'new_bands_1KM_Emissive:MODIS_SWATH_Type_L1B'
 
-    # Process Reflective bands
-    cursor = 0
-    size = raw_data['EV_1KM_Emissive'][0].shape
-    upscale_sza = da.repeat(da.repeat(level1.sza.data,5,axis=0),5,axis=1)
-    upscale_sza = upscale_sza[:size[0],:size[1]].rechunk(chunks=500)
-    for var,new in zip(old_varname[:-1], new_varname[:-1]):
-        ref_b   = raw_data[var]
-        scales  = ref_b.attrs[tag_scale]
-        offsets = ref_b.attrs[tag_offset]
-        unit    = ref_b.attrs[tag_unit]
-        for i,band in enumerate(ref_b):
-            band.attrs = {}
-            level1[new+f'_{cursor+1}'] = scales[i] * (band - offsets[i])
-            level1[new+f'_{cursor+1}'].attrs['unit'] = unit
-            if radiometry == 'reflectance':
-                level1[new+f'_{cursor+1}'] /= da.cos(da.radians(upscale_sza))
-            cursor += 1
-    if not split:
-        level1 = merge(level1, dim=f'bands_vis', pattern=r'(.+)_(\d+)')
-        level1[new].attrs['unit'] = unit
-    
-    # Process Emissive bands
-    emi_b   = raw_data['EV_1KM_Emissive']
-    scales  = emi_b.attrs['radiance_scales']
-    offsets = emi_b.attrs['radiance_offsets']
-    unit    = emi_b.attrs['radiance_units']
-    for i,band in enumerate(emi_b):
-        band.attrs = {}
-        level1[bt+f'_{i+1}'] = scales[i] * (band - offsets[i])
-        level1[bt+f'_{i+1}'].attrs['unit'] = 'Kelvin'
-        if radiometry == 'reflectance':
-            level1[bt+f'_{i+1}'] = calibrate_bt(level1[bt+f'_{i+1}'],i)
-    if not split:
-        level1 = merge(level1, dim='bands_bt', pattern=r'(.+)_(\d+)')
-        level1[bt].attrs['unit'] = 'Kelvin' if radiometry == 'reflectance' else unit
-        level1[n.flags] = level1[bt].isel(bands_bt=0).isnull().astype(n.flags_dtype)
-    
-    level1 = level1.drop_indexes(list(level1.coords)) \
-                   .reset_coords(drop=True)
-    
+    scales = xr.DataArray(emi_b.radiance_scales, dims=band_dim)
+    offsets = xr.DataArray(emi_b.radiance_offsets, dims=band_dim)
+
+    # Expand to match dimensions of emi_b
+    scales = scales.broadcast_like(emi_b)
+    offsets = offsets.broadcast_like(emi_b)
+
+    nd = scales * (emi_b - offsets)
+    nd.attrs['desc'] = n.bt.desc
+    nd.attrs['unit'] = n.bt.unit
+    nd.attrs['minval'] = n.bt.minv
+    nd = nd.rename({band_dim : "bands_bt"})
+    level1[n.bt.name] = nd
+
     return level1
 
 def supplement_latlon(l1, chunks): 
