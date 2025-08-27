50232d0e192bc8ae79b4f108fcf0b94ea10e9696
François Steinmetz <fs@hygeos.com>
2025-08-27 22:33:03 +0200

    Partially revert "Fixs: remove modules that have been replaced by SAND and HARP modules"
    This partially reverts commit 5c2e0dbbc9dc4779a81fe4792110a08dab45d07a.
    
    We keep some modules temporarily for backward compatibility.

---
+++ tests/test_ancillary.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from datetime import datetime as dt, timedelta
+from matplotlib import pyplot as plt
+
+import pytest
+
+from eoread.era5 import ERA5
+from eoread.ancillary_nasa import Ancillary_NASA, open_NASA
+from eoread.meris import Level1_MERIS
+from eoread.sample_products import get_sample_products
+from core.tools import datetime
+from . import conftest
+
+p = get_sample_products()
+
+
+
+@pytest.fixture(params=[
+    'total_column_ozone',
+    'sea_level_pressure',
+    'horizontal_wind'])
+def variable(request):
+    return request.param
+
+@pytest.mark.parametrize('ancillary,args', [
+        (ERA5, [dt(2010, 1, 1, 1, 0, 0)]),
+        (Ancillary_NASA, [dt(2023,1,1,0,0,0),
+                          'GMAO_MERRA2.%Y%m%dT000000.MET.nc']),
+        ])
+def test_download(request, args, ancillary, variable):
+    """
+    Download a single file
+    """
+    f = ancillary().download(*args)
+    anc = open_NASA(f)
+
+    # check that it is properly wrapped
+    assert anc.latitude.min() == -90
+    assert anc.latitude.max() == 90
+    assert anc.longitude.min() == -180
+    assert anc.longitude.max() == 180
+    assert (anc[variable].isel(longitude=0) == anc[variable].isel(longitude=-1)).all()
+    assert 'time' not in anc.dims
+
+    anc[variable].plot()
+    plt.title(variable)
+    conftest.savefig(request)
+
+
+@pytest.mark.parametrize('ancillary,date', [
+        (ERA5, dt(2010, 1, 1, 1, 30, 0)),
+        (Ancillary_NASA, dt.now()-timedelta(hours=12)),
+        (Ancillary_NASA, dt(2005, 1, 1, 0, 0, 0)),
+        (Ancillary_NASA, dt(2010, 1, 1, 11, 11, 11)),
+        ])
+def test_get_single(request, date, ancillary, variable):
+    """
+    Download and interpolate between bracketing files
+    """
+    anc = ancillary().get(date)
+    assert not 'time' in anc.dims
+    anc[variable].plot()
+    plt.title(variable)
+    conftest.savefig(request)
+
+
+@pytest.mark.parametrize('ancillary', [ERA5, Ancillary_NASA])
+@pytest.mark.parametrize('method', ['nearest', 'linear'])
+def test_interp(request, method, ancillary, variable):
+    """
+    Fetch and interpolate ancillary data for a MERIS product
+
+    We test with an orbit that crosses the date line
+    """
+    def get_unit(da):
+        if 'units' in da.attrs:
+            return da.units
+        elif 'unit' in da.attrs:
+            return da.unit
+        else:
+            return '<unit not available>'
+        
+    l1 = Level1_MERIS(p['prod_meris_L1_20080701']['path']).sel(
+        y=slice(2000, 2600),
+        )
+
+    anc = ancillary().get(datetime(l1))
+    figsize = (7, 7)
+
+    # plot longitude
+    plt.figure(figsize=figsize)
+    plt.imshow(l1.longitude)
+    plt.title('Longitude')
+    plt.colorbar()
+    conftest.savefig(request)
+
+    # Plot embedded MERIS
+    plt.figure(figsize=figsize)
+    plt.imshow(l1[variable])
+    plt.title(f'{variable} ({get_unit(l1[variable])}) | MERIS')
+    plt.colorbar()
+    conftest.savefig(request)
+
+    # Plot interpolated data (in time and lat/lon)
+    interpolated = anc[variable].compute().interp(
+        latitude=l1.latitude,
+        longitude=l1.longitude,
+        method=method,
+    )
+    plt.figure(figsize=figsize)
+    plt.imshow(interpolated)
+    plt.colorbar()
+    plt.title(f'{variable} ({get_unit(anc[variable])}) | ERA5')
+    conftest.savefig(request)
